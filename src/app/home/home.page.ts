c4e70543dbb322abe9c034937ff4009f108ee727
DavidSaulFlores <dasafloresme@ittepic.edu.mx>
2021-01-09 11:10:22 -0700

    vista

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Estudiante } from '../models/estudiante';
+import { EstudianteService } from '../services/estudiante.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public students: Estudiante[];
+
+  constructor(private service: EstudianteService) {
+    this.service.getStudent().subscribe(data => {
+      this.students = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data() as Estudiante
+        };
+      });
+    });
+  }
+
+  update(student: Estudiante, active: boolean){
+    student.active = active;
+    this.service.updateStudent(student, student.id);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyD92V0yGEcsPiKR_mZkRVTJkjG_SJ3ileA',
+    authDomain: 'damcrudfirebase1-c9d28.firebaseapp.com',
+    projectId: 'damcrudfirebase1-c9d28',
+    storageBucket: 'damcrudfirebase1-c9d28.appspot.com',
+    messagingSenderId: '142397667342',
+    appId: '1:142397667342:web:a848de3134ce0e0e2ba45f'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
